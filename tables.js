dc784476ab6b0054e6063c92b86fa446b3be528b
Paul Mack <mackp@cs.uchicago.edu>
2017-05-10 11:16:28 -0500

    adding tables

--- tables.js
+++ tables.js
@@ -7,3 +7,14 @@ var Num_LtHsEd = {name: 'Num_LtHsEd', label: 'Less Than High School Education', 
 // Tables
 var eligible = {name: 'eligible', vars: [Num_Kids_A0to2, Num_Kids_A3to4], qId: "eligibleQuestion", qText: "How many children are eligible for our program?"}
 var need = {name: 'need', vars: [Num_Kids_AllParentsWorking, Num_LtHsEd], qId: "needQuestion", qText: "What needs are in the community?"}
+
+// Tables
+var numChildren = {name: 'numChildren'], vars: [Num_Kids_A0to5, Num_Kids_A0to2, Num_Kids_A3to4, headers:["Relability Indicator", "Estimated Number", "Percent of All Children"]}
+var fplALt6Children = {name: 'fplLt6Children', vars: [Num_IncRatKidsLt6_Lt50,Num_IncRatKidsLt6_Lt100,Num_IncRatKidsLt6_Lt185], headers: ["Relability Indicator", "Estimated Number", "Percent of All Children Age 0 to 5"]}
+var fplALt3Children = {name: 'fplLt3Children', vars:[], headers: ["Relability Indicator", "Estimated Number", "Percent of All Children Age 0 to 3"]}
+var fplA3to5Children = {name: 'fplA3to5Children', vars: [], headers: ["Relability Indicator", "Estimated Number", "Percent of All Children 3 to 5"]}
+var parentsWorking = {name: 'parentsWorking', vars: [], headers: ["Relability Indicator", "Estimated Number", "Percent of All Households with Children Age 0-6"]}
+var fplHH = {name: 'fplHH', vars: [], headers: ["Relability Indicator", "Estimated Number", "Percent of All Households Below 100% FPL"]}
+var noCarHH = {name: 'noCarHH', vars: [], headers: ["Relability Indicator", "Estimated Number", "Percent of All Children"]}
+var crime = {name: 'crime': vars: [], headers: ["Relability Indicator", "Rate per 1,000 Residents"]}
+var ftCare = {name: 'ftCare', vars: [], headers: ["Relability Indicator", "Estimated Number", "Percent of All Households with Children Age 0-6"]}
