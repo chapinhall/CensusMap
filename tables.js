616dff51de2c3927885116d9f222914c0ea33777
Paul Mack <mackp@cs.uchicago.edu>
2017-05-10 20:31:12 -0500

    fixed typo

--- tables.js
+++ tables.js
@@ -1,8 +1,8 @@
 // Statistics
-var Num_Kids_A0to2 = {name: 'Num_Kids_A0to2', label: 'Children Age 0 to 2', souce: 'ACS 2011 -2015'}
-var Num_Kids_A3to4 = {name: 'Num_Kids_A3to4', label: 'Children Age 3 to 4', souce: 'ACS 2011 -2015'}
-var Num_Kids_AllParentsWorking = {name: 'Num_Kids_AllParentsWorking', label: 'Number of Children in Households where Both Parents Work', souce: 'ACS 2011 -2015'}
-var Num_LtHsEd = {name: 'Num_LtHsEd', label: 'Less Than High School Education', souce: 'ACS 2011 -2015'}
+var Num_Kids_A0to2 = {name: 'Num_Kids_A0to2', label: 'Children Age 0 to 2', source: 'ACS 2011 -2015'}
+var Num_Kids_A3to4 = {name: 'Num_Kids_A3to4', label: 'Children Age 3 to 4', source: 'ACS 2011 -2015'}
+var Num_Kids_AllParentsWorking = {name: 'Num_Kids_AllParentsWorking', label: 'Number of Children in Households where Both Parents Work', source: 'ACS 2011 -2015'}
+var Num_LtHsEd = {name: 'Num_LtHsEd', label: 'Less Than High School Education', source: 'ACS 2011 -2015'}
 
 // Tables
 var eligible = {name: 'eligible', vars: [Num_Kids_A0to2, Num_Kids_A3to4], qId: "eligibleQuestion", qText: "How many children are eligible for our program?"}
