56f232608b8c2c16e73fb25e23b77b459290ff03
Paul Mack <mackp@cs.uchicago.edu>
2017-05-22 16:23:06 -0500

    adding in special formatting

--- tables.js
+++ tables.js
@@ -23,9 +23,13 @@ source: 'American Community Survey (ACS)', unit: 'tract', pctLabel: 'This is the
 var nChLt6_2Par_Lf = {name: 'nChLt6_2Par_Lf', label: 'Two parent households with children under age 6, where both parents work',
 source: 'American Community Survey (ACS)', unit: 'tract', pctLabel: 'This is the number of children aged 6 or less in two-parent households where both parents work, as a percentage of all children aged 6 or less in two-parent households'}
 
+var nAdultsGe25_LtHsEd = {name: 'nAdultsGe25_LtHsEd', label: 'Adults aged 25 or more, with less than a high school education',
+source: 'American Community Survey (ACS)', unit: 'tract', pctLabel: 'This is the number of adults aged 25 or more who have not earned at least a high school diploma (or equivalent), as a percentage of all adults aged 25 or more'}
 
+var violCrimes_per1k = {name: 'violCrimes_per1k', label: 'This is the ratio between the number of crimes, and the number of residents (in thousands)', unit: 'tract',
+source: '	City of Chicago Data Portal', specialFormat: true}
 
 // Tables
-var need = {name: 'need', vars: [nBirths_Teen, nHhs_Rent, nRenters_Ge30Pct, nKidsLt6_GpCare], qId: "needQuestion", qText: "What needs are in the community?"}
+var need = {name: 'need', vars: [violCrimes_per1k, nBirths_Teen, nHhs_Rent, nRenters_Ge30Pct, nKidsLt6_GpCare, nAdultsGe25_LtHsEd], qId: "needQuestion", qText: "What needs are in the community?"}
 var enroll = {name: 'enroll', vars: [nFamLtPov_Ge3OwnKids, nHhSizeCars_Ge3PerHh_0Cars], qId: "enrollQuestion", qText: "How many families are likely to enroll in community-based programs / school based programs?"}
 var care = {name: 'care', vars: [nChLt6_1Par_Lf, nChLt6_2Par_Lf], qId: "careQuestion", qText: "How many children need full time care?"}
