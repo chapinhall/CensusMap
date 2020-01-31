5fda7639c89e3f6943f5f7515c3b40c939f74300
nmader <nmader@chapinhall.org>
2020-01-30 22:51:27 -0600

    Updated census.py script to allow for direct call of merge function, rather than allowing for/requiring specification at command line

--- data_process/census.py
+++ data_process/census.py
@@ -23,40 +23,41 @@ def read_json(geojson_filename):
     print('Geojson file has {} rows to merge'.format(len(geojson['features'])))
     return geojson
 
-def merge(args):
-    df = read_csv(args.csv_filename, args.id_csv_col)
-    geojson = read_json(args.geojson_filename)
+def merge(csv_filename, geojson_filename, id_csv_col, id_geojson_col, output_name):
+    df = read_csv(csv_filename, id_csv_col)
+    geojson = read_json(geojson_filename)
     # Iniitialize unmatched records
     unmatched_json_records = []
     for feature in geojson['features']:
-        unmatched_json_records.append(feature['properties'][args.id_geojson_col])
+        unmatched_json_records.append(feature['properties'][id_geojson_col])
     matched = 0
     for row in df.itertuples(index=False):
         for feature in geojson['features']:
             #the first position of the df is the id column
-            if str(row[0]) == str(feature['properties'][args.id_geojson_col]):
+            if str(row[0]) == str(feature['properties'][id_geojson_col]):
                 matched += 1
                 unmatched_json_records.remove(str(row[0]))
                 colnames = list(df.columns)
                 for i in range(len(row)):
                     feature['properties'][colnames[i]] = str(row[i])
     print('Matched {} records'.format(matched))
-    write_unmatched(unmatched_json_records,args)
+    write_unmatched(unmatched_json_records, csv_filename)
 
     # save merged datasets to an output geojson file
     json_string = json.dumps(geojson)
-    print('Saving file to {}'.format(args.output_name))
-    with open(args.output_name, 'w') as f:
+    print('Saving file to {}'.format(output_name))
+    with open(output_name, 'w') as f:
         f.write(json_string)
     f.close()
 
-def write_unmatched(unmatched_json_records, args):
-    csv_out = args.csv_filename.split("/")[-1].split(".")[0] + "_unmatched.csv"
+def write_unmatched(unmatched_json_records, csv_filename):
+    csv_out = csv_filename.split("/")[-1].split(".")[0] + "_unmatched.csv"
     with open(csv_out,'w', newline='') as f:
         writer =csv.writer(f)
         writer.writerows([unmatched_json_records])
     print("Unmatched csv records written to {}".format(csv_out))
 
+'''
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='Merge geojson file with attribute csv file.')
     parser.add_argument('csv_filename', type=str,
@@ -73,3 +74,10 @@ if __name__=='__main__':
     args = parser.parse_args()
     print('Merging  {} and {}'.format(args.csv_filename, args.geojson_filename))
     merge(args)
+'''
+
+merge("./tract-level-indicators-for-cnat.csv", "./tracts_clipped.geojson", "geoid", "geoid10", "./tracts_clipped_withAttr.geojson")
+
+merge("./chapin-hall-estimates-of-program-eligible-youth-by-community-area.csv", "./Boundaries - Community Areas (current).geojson",
+       "cca", "area_numbe", "./commAreas_withAttr.geojson")
+
